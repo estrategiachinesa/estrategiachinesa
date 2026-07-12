66b7d472040ea863d759bf89c0f8ef56b4ac30ca
estrategiachinesa <estrategiachinesa@gmail.com>
2026-07-12 01:33:25 -0300

    v1.0

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 
 import type { NextConfig } from 'next';
 
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+const repo = process.env.GITHUB_REPOSITORY 
+  ? process.env.GITHUB_REPOSITORY.split('/')[1] 
+  : 'estrategiachinesa';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  trailingSlash: true,
+  basePath: isGithubActions ? `/${repo}` : '',
   images: {
     unoptimized: true,
     remotePatterns: [
